b0e2965014aa5a2edcb856e516d453810bfc36d4
Andreas Reich <r_andreas2@web.de>
2020-01-15 22:17:56 +0100

    particle density update

--- src/hydroparticles.rs
+++ src/hydroparticles.rs
@@ -2,7 +2,8 @@ use super::units::*;
 use ggez::graphics::Rect;
 use ggez::nalgebra as na;
 
-use crate::smoothing_kernel::{Kernel, Poly6};
+use crate::smoothing_kernel;
+use crate::smoothing_kernel::Kernel;
 
 pub struct HydroParticles {
     pub positions: Vec<Position>,
@@ -10,13 +11,13 @@ pub struct HydroParticles {
 
     pub boundary_particles: Vec<Position>, // also called "shadow particles", immovable particles used for boundaries
 
-    smoothing_length: f32, // typically expressed as 'h'
+    //smoothing_length: f32, // typically expressed as 'h'
     particle_density: f32, // #particles/m² for resting fluid
     fluid_density: f32,    // kg/m² for the resting fluid
 
-    pressure_kernel: Poly6,
+    density_kernel: smoothing_kernel::Poly6,
 
-    densities: Vec<f32>,
+    pub densities: Vec<f32>,
 }
 
 impl HydroParticles {
@@ -29,17 +30,17 @@ impl HydroParticles {
             positions: Vec::new(),
             velocities: Vec::new(),
             boundary_particles: Vec::new(),
-            smoothing_length: smoothing_length,
+            //smoothing_length: smoothing_length,
             particle_density: particle_density,
             fluid_density: fluid_density,
-            pressure_kernel: Poly6::new(smoothing_length),
+            density_kernel: smoothing_kernel::Poly6::new(smoothing_length),
             densities: Vec::new(),
         }
     }
 
-    // fn particle_mass(&self) -> f32 {
-    //     self.particle_density / self.fluid_density
-    // }
+    fn particle_mass(&self) -> f32 {
+        self.particle_density / self.fluid_density
+    }
 
     fn num_particles_per_meter(&self) -> f32 {
         self.particle_density.sqrt()
@@ -85,16 +86,38 @@ impl HydroParticles {
         }
     }
 
-    // fn update_densities(&mut self) {
-    //     assert_eq!(self.positions.len(), self.densities.len());
+    fn update_densities(&mut self) {
+        assert_eq!(self.positions.len(), self.densities.len());
 
-    //     for density in self.densities.iter_mut() {
-    //         self.pressure_kernel.evaluate(1.0);
-    //     }
-    // }
+        for density in self.densities.iter_mut() {
+            *density = 0.0;
+        }
+
+        // Density contributions are symmetric!
+        for (i, posi) in self.positions.iter().enumerate() {
+            for (j, posj) in self.positions.iter().enumerate().skip(i + 1) {
+                let distance_sq = na::distance_squared(posi, posj);
+                let density_contribution = self.density_kernel.evaluate(distance_sq);
+                self.densities[i] += density_contribution;
+                self.densities[j] += density_contribution;
+            }
+            for posj in self.boundary_particles.iter() {
+                let distance_sq = na::distance_squared(posi, posj);
+                let density_contribution = self.density_kernel.evaluate(distance_sq);
+                self.densities[i] += density_contribution;
+            }
+        }
+
+        let mass = self.particle_mass();
+        for density in self.densities.iter_mut() {
+            *density *= mass;
+        }
+    }
 
     pub fn physics_step(&mut self, dt: f32) {
         assert_eq!(self.positions.len(), self.velocities.len());
+
+        self.update_densities();
 
         let gravity = na::Vector2::new(0.0, -9.81);
 
