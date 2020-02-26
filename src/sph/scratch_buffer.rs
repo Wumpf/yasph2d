4614560e6a8d06c4ab05cb45856aa940aecb0ee1
Andreas Reich <r_andreas2@web.de>
2020-02-26 15:04:43 +0100

    Scratch buffers does now type conversions for its internal buffers .. just for fun. there's no real profit in this one right now

--- src/sph/scratch_buffer.rs
+++ src/sph/scratch_buffer.rs
@@ -1,13 +1,14 @@
 use crate::units::*;
 use cgmath::prelude::*;
+use std::rc::Rc;
 use std::cell::RefCell;
 
-pub struct ScratchBuffer<'a, T: Copy> {
+pub struct ScratchBuffer<T: Copy, TStorage: Copy> {
     pub buffer: Vec<T>,
-    store: &'a RefCell<ScratchBufferTypeStore<T>>,
+    store: Rc<RefCell<ScratchBufferTypeStore<TStorage>>>,
 }
 
-impl<'a, T: Copy> Drop for ScratchBuffer<'a, T> {
+impl<'a, T: Copy, TStorage: Copy> Drop for ScratchBuffer<T, TStorage> {
     fn drop(&mut self) {
         let mut new_buffer_data_owner = Vec::new();
         std::mem::swap(&mut new_buffer_data_owner, &mut self.buffer);
@@ -19,59 +20,72 @@ struct ScratchBufferTypeStore<T: Copy> {
     buffers: Vec<Vec<T>>,
 }
 
-impl<T: Copy> ScratchBufferTypeStore<T> {
-    fn new() -> ScratchBufferTypeStore<T> {
-        ScratchBufferTypeStore::<T> { buffers: Vec::new() }
+impl<TStorage: Copy> ScratchBufferTypeStore<TStorage> {
+    fn new() -> ScratchBufferTypeStore<TStorage> {
+        ScratchBufferTypeStore::<TStorage> { buffers: Vec::new() }
     }
 
-    fn get_buffer(&mut self, size: usize, default: T) -> Vec<T> {
+    fn get_buffer<T: Copy>(&mut self, size: usize, default: T) -> Vec<T> {
+        assert_eq!(std::mem::size_of::<TStorage>(), std::mem::size_of::<T>());
+        assert_eq!(std::mem::align_of::<TStorage>(), std::mem::align_of::<T>());
+
         match self.buffers.pop() {
-            Some(mut buffer) => {
-                buffer.resize(size, default);
-                buffer
+            Some(buffer) => {
+                let mut buffer = std::mem::ManuallyDrop::new(buffer);
+                let mut new_buffer = unsafe { Vec::from_raw_parts(buffer.as_mut_ptr() as *mut T, buffer.len(), buffer.capacity()) };
+                new_buffer.resize(size, default);
+                new_buffer
             }
             None => vec![default; size],
         }
     }
 
-    fn return_buffer(&mut self, buffer: Vec<T>) {
-        self.buffers.push(buffer);
+    fn return_buffer<T: Copy>(&mut self, buffer: Vec<T>) {
+        let mut buffer = std::mem::ManuallyDrop::new(buffer);
+        let new_buffer = unsafe { Vec::from_raw_parts(buffer.as_mut_ptr() as *mut TStorage, buffer.len(), buffer.capacity()) };
+        self.buffers.push(new_buffer);
     }
 }
 
 pub struct ScratchBufferStore {
-    buffers_real: RefCell<ScratchBufferTypeStore<Real>>,
-    buffers_vector: RefCell<ScratchBufferTypeStore<Vector>>,
-    buffers_point: RefCell<ScratchBufferTypeStore<Point>>, // todo, do some unsafe code to fuse this with Vector store
+    buffers_real: Rc<RefCell<ScratchBufferTypeStore<Real>>>,
+    buffers_vector: Rc<RefCell<ScratchBufferTypeStore<Vector>>>,
 }
 
+#[allow(clippy::new_without_default)]
 impl ScratchBufferStore {
     pub fn new() -> ScratchBufferStore {
         ScratchBufferStore {
-            buffers_real: RefCell::new(ScratchBufferTypeStore::new()),
-            buffers_vector: RefCell::new(ScratchBufferTypeStore::new()),
-            buffers_point: RefCell::new(ScratchBufferTypeStore::new()),
+            buffers_real: Rc::new(RefCell::new(ScratchBufferTypeStore::new())),
+            buffers_vector: Rc::new(RefCell::new(ScratchBufferTypeStore::new())),
         }
     }
 
-    pub fn get_buffer_real(&mut self, size: usize) -> ScratchBuffer<Real> {
-        ScratchBuffer::<Real> {
+    pub fn get_buffer_real(&self, size: usize) -> ScratchBuffer<Real, Real> {
+        ScratchBuffer::<Real, Real> {
             buffer: self.buffers_real.borrow_mut().get_buffer(size, 0.0),
-            store: &self.buffers_real,
+            store: Rc::clone(&self.buffers_real),
         }
     }
 
-    pub fn get_buffer_vector(&mut self, size: usize) -> ScratchBuffer<Vector> {
-        ScratchBuffer::<Vector> {
+    pub fn get_buffer_uint(&self, size: usize) -> ScratchBuffer<u32, Real> {
+        ScratchBuffer::<u32, Real> {
+            buffer: self.buffers_real.borrow_mut().get_buffer(size, 0),
+            store: Rc::clone(&self.buffers_real),
+        }
+    }
+
+    pub fn get_buffer_vector(&self, size: usize) -> ScratchBuffer<Vector, Vector> {
+        ScratchBuffer::<Vector, Vector> {
             buffer: self.buffers_vector.borrow_mut().get_buffer(size, Vector::zero()),
-            store: &self.buffers_vector,
+            store: Rc::clone(&self.buffers_vector),
         }
     }
 
-    pub fn get_buffer_point(&mut self, size: usize) -> ScratchBuffer<Point> {
-        ScratchBuffer::<Point> {
-            buffer: self.buffers_point.borrow_mut().get_buffer(size, Point::origin()),
-            store: &self.buffers_point,
+    pub fn get_buffer_point(&self, size: usize) -> ScratchBuffer<Point, Vector> {
+        ScratchBuffer::<Point, Vector> {
+            buffer: self.buffers_vector.borrow_mut().get_buffer(size, Point::origin()),
+            store: Rc::clone(&self.buffers_vector),
         }
     }
 }
