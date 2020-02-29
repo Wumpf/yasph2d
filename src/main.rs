15b8e7880d822003afa3659a8ee215cf90a257cb
Andreas Reich <r_andreas2@web.de>
2020-02-29 11:12:24 +0100

    remoddeld main entry point added recording mode, doesn't do anything yet

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use cgmath::prelude::*;
-use ggez::event::{self, EventHandler, KeyCode};
+use ggez::event::{self, EventHandler, KeyCode, KeyMods};
 use ggez::graphics::Rect;
-use ggez::input::keyboard;
 use ggez::{conf, graphics, timer, Context, GameResult};
+use microprofile;
 use std::collections::VecDeque;
 use std::time::{Duration, Instant};
 
@@ -11,8 +11,6 @@ mod camera;
 use camera::*;
 use yasph2d::sph::*;
 use yasph2d::units::*;
-
-use microprofile;
 
 fn main() -> GameResult {
     let context_builder = ggez::ContextBuilder::new("YaSPH2D", "AndreasR")
@@ -32,7 +30,15 @@ fn main() -> GameResult {
     event::run(ctx, event_loop, state)
 }
 
+#[derive(PartialEq)]
+enum UpdateMode {
+    RealTime,
+    Recording,
+}
+
 struct MainState {
+    update_mode: UpdateMode,
+
     fluid_world: FluidParticleWorld,
     sph_solver: Box<dyn Solver>,
 
@@ -40,14 +46,40 @@ struct MainState {
     particle_mesh: graphics::Mesh,
 
     simulation_step_duration_history: VecDeque<Duration>,
-    simulation_processing_time: Duration,
-    simulationstep_count: u32,
+    simulation_processing_time_frame: Duration,
+    simulationstep_count_frame: u32,
 
-    total_simulation_time: Duration,
-    total_simulation_processing_time: Duration,
+    simulation_starttime: Instant,
+    simulation_realtime_total: Duration,
+    simulation_processing_time_total: Duration,
 }
 
 const SIMULATION_STEP_HISTORY_LENGTH: usize = 80;
+
+const REALTIME_TO_SIMTIME: f32 = 1.0;
+const NUM_DESIRED_SIM_UPDATES_PER_SECOND: u32 = 60 * 20;
+const SIM_TIME_STEP: Real = REALTIME_TO_SIMTIME / (NUM_DESIRED_SIM_UPDATES_PER_SECOND as Real);
+// Number of seconds simulation is allowed to process before slowing down physics time.
+const MAX_ALLOWED_SIM_PROCESSING_TIME: Real = 1.0 / 10.0; // if render takes 0 time this would result in min 10fps (since it doesn't, real min fps is lower)
+
+fn clamp(v: f32, min: f32, max: f32) -> f32 {
+    if v < min {
+        min
+    } else if v > max {
+        max
+    } else {
+        v
+    }
+}
+
+fn heatmap_color(t: f32) -> graphics::Color {
+    graphics::Color {
+        r: clamp(t * 3.0, 0.0, 1.0),
+        g: clamp(t * 3.0 - 1.0, 0.0, 1.0),
+        b: clamp(t * 3.0 - 2.0, 0.0, 1.0),
+        a: 1.0,
+    }
+}
 
 impl MainState {
     pub fn new(ctx: &mut Context) -> MainState {
@@ -77,6 +109,7 @@ impl MainState {
         .unwrap();
 
         MainState {
+            update_mode: UpdateMode::RealTime,
             fluid_world,
             sph_solver: Box::new(sph_solver),
 
@@ -84,15 +117,16 @@ impl MainState {
             particle_mesh,
 
             simulation_step_duration_history: VecDeque::with_capacity(SIMULATION_STEP_HISTORY_LENGTH),
-            simulation_processing_time: Default::default(),
-            simulationstep_count: 0,
+            simulation_processing_time_frame: Default::default(),
+            simulationstep_count_frame: 0,
 
-            total_simulation_time: Default::default(),
-            total_simulation_processing_time: Default::default(),
+            simulation_starttime: Instant::now(),
+            simulation_realtime_total: Default::default(),
+            simulation_processing_time_total: Default::default(),
         }
     }
 
-    pub fn reset_fluid(fluid_world: &mut FluidParticleWorld) {
+    fn reset_fluid(fluid_world: &mut FluidParticleWorld) {
         fluid_world.remove_all_fluid_particles();
         fluid_world.remove_all_boundary_particles();
 
@@ -103,75 +137,147 @@ impl MainState {
 
         fluid_world.add_boundary_line(Point::new(0.0, 0.6), Point::new(1.75, 0.5));
     }
-}
 
-fn clamp(v: f32, min: f32, max: f32) -> f32 {
-    if v < min {
-        min
-    } else if v > max {
-        max
-    } else {
-        v
+    fn draw_text(&mut self, ctx: &mut Context) -> GameResult {
+        microprofile::scope!("MainState", "text");
+
+        let fps = timer::fps(ctx);
+        let average_simulation_step_duration =
+            self.simulation_step_duration_history.iter().sum::<Duration>() / self.simulation_step_duration_history.len() as u32;
+
+        let simulation_info_text = format!(
+            "Sim duration: {:3.2}ms ({:4} steps)| Single Step (averaged over {}): {:.2}ms\nTotal Simulated Time {:.2}\nTotal Processing Time {:.2}",
+            self.simulation_processing_time_frame.as_secs_f64() * 1000.0,
+            self.simulationstep_count_frame,
+            self.simulation_step_duration_history.len(),
+            average_simulation_step_duration.as_secs_f64() * 1000.0,
+            self.simulation_realtime_total.as_secs_f64(),
+            self.simulation_processing_time_total.as_secs_f64(),
+        );
+
+        let fps_display = graphics::Text::new(match self.update_mode {
+            UpdateMode::RealTime => format!(
+                "{:3.2}ms, FPS: {:3.2}, passed time {:.2}s\n{}",
+                1000.0 / fps,
+                fps,
+                (Instant::now() - self.simulation_starttime).as_secs_f64(),
+                simulation_info_text,
+            ),
+
+            UpdateMode::Recording => format!("RECORDING\n{}", simulation_info_text,),
+        });
+        graphics::draw(ctx, &fps_display, (RenderPoint::new(10.0, 10.0), graphics::WHITE))?;
+
+        if self.update_mode == UpdateMode::RealTime && self.simulation_processing_time_frame.as_secs_f32() > MAX_ALLOWED_SIM_PROCESSING_TIME {
+            graphics::draw(
+                ctx,
+                &graphics::Text::new("REALTIME OFF (max sim processing time hit)"),
+                (RenderPoint::new(10.0, 70.0), graphics::Color::new(1.0, 0.2, 0.2, 1.0)),
+            )?;
+        }
+
+        Ok(())
+    }
+
+    fn draw_fluid(&mut self, ctx: &mut Context) -> GameResult {
+        microprofile::scope!("MainState", "draw fluid");
+
+        let boundary_color = graphics::Color {
+            r: 0.2,
+            g: 0.2,
+            b: 0.2,
+            a: 1.0,
+        };
+        for (p, a) in self
+            .fluid_world
+            .particles
+            .positions
+            .iter()
+            .zip(self.fluid_world.particles.velocities.iter())
+        {
+            let c = heatmap_color((a.magnitude() * 0.1) as f32);
+            let rp: RenderPoint = RenderPoint::new(p.x, p.y);
+            graphics::draw(ctx, &self.particle_mesh, ggez::graphics::DrawParam::default().dest(rp).color(c))?;
+        }
+        for p in self.fluid_world.particles.boundary_particles.iter() {
+            let rp: RenderPoint = RenderPoint::new(p.x, p.y);
+            graphics::draw(
+                ctx,
+                &self.particle_mesh,
+                ggez::graphics::DrawParam::default().dest(rp).color(boundary_color),
+            )?;
+        }
+
+        graphics::pop_transform(ctx);
+        graphics::apply_transformations(ctx)?;
+        Ok(())
+    }
+
+    fn single_sim_step(&mut self, current_time: &mut Instant) {
+        let time_step_start = *current_time;
+
+        self.sph_solver.simulation_step(&mut self.fluid_world, SIM_TIME_STEP);
+        *current_time = Instant::now();
+
+        self.simulation_processing_time_frame = *current_time - time_step_start;
+        self.simulation_processing_time_total += self.simulation_processing_time_frame;
+        self.simulation_realtime_total += Duration::from_secs_f64(SIM_TIME_STEP as f64);
+        self.simulationstep_count_frame += 1;
+
+        if self.simulation_step_duration_history.len() == SIMULATION_STEP_HISTORY_LENGTH {
+            self.simulation_step_duration_history.pop_front();
+        }
+        self.simulation_step_duration_history.push_back(self.simulation_processing_time_frame);
     }
 }
-
-fn heatmap_color(t: f32) -> graphics::Color {
-    graphics::Color {
-        r: clamp(t * 3.0, 0.0, 1.0),
-        g: clamp(t * 3.0 - 1.0, 0.0, 1.0),
-        b: clamp(t * 3.0 - 2.0, 0.0, 1.0),
-        a: 1.0,
-    }
-}
-
-const REALTIME_TO_SIMTIME: f32 = 1.0;
-const NUM_DESIRED_SIM_UPDATES_PER_SECOND: u32 = 60 * 20;
-const SIM_TIME_STEP: Real = REALTIME_TO_SIMTIME / (NUM_DESIRED_SIM_UPDATES_PER_SECOND as Real);
-// Number of seconds simulation is allowed to process before slowing down physics time.
-const MAX_ALLOWED_SIM_PROCESSING_TIME: Real = 1.0 / 10.0; // if render takes 0 time this would result in min 10fps (since it doesn't, real min fps is lower)
 
 impl EventHandler for MainState {
+    fn key_down_event(&mut self, ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods, _repeat: bool) {
+        match keycode {
+            KeyCode::Escape => {
+                ggez::event::quit(ctx);
+            }
+
+            KeyCode::Space => {
+                self.sph_solver.clear_cached_data(); // todo: this is super meh
+                self.simulation_starttime = Instant::now();
+                self.simulation_realtime_total = Default::default();
+                self.simulation_processing_time_total = Default::default();
+                Self::reset_fluid(&mut self.fluid_world);
+            }
+            KeyCode::R => {
+                if !_repeat {
+                    self.update_mode = if self.update_mode == UpdateMode::RealTime {
+                        UpdateMode::Recording
+                    } else {
+                        UpdateMode::RealTime
+                    };
+                }
+            }
+            _ => {
+                self.update_mode = UpdateMode::RealTime;
+            }
+        }
+    }
+
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         microprofile::scope!("MainState", "update");
 
-        self.simulationstep_count = 0;
-
-        if keyboard::is_key_pressed(ctx, KeyCode::Space) {
-            self.sph_solver.clear_cached_data(); // todo: this is super meh
-            self.total_simulation_time = Default::default();
-            self.total_simulation_processing_time = Default::default();
-            Self::reset_fluid(&mut self.fluid_world);
-        }
+        self.simulationstep_count_frame = 0;
 
         let mut current_time = Instant::now();
-        let time_sim_start = current_time;
-        self.simulation_processing_time = Duration::from_secs(0);
+        self.simulation_processing_time_frame = Duration::from_secs(0);
         while timer::check_update_time(ctx, NUM_DESIRED_SIM_UPDATES_PER_SECOND) {
-            let time_step_start = current_time;
-
-            // if self.total_simulation_time > Duration::from_secs(2) {
+            // if self.simulation_realtime_total > Duration::from_secs(2) {
             //     break;
             // }
 
-            self.sph_solver.simulation_step(&mut self.fluid_world, SIM_TIME_STEP);
-            current_time = Instant::now();
-            let step_processing_time = current_time - time_step_start;
-
-            self.simulation_processing_time = current_time - time_sim_start;
-            self.total_simulation_processing_time += step_processing_time;
-            self.total_simulation_time += Duration::from_secs_f64(SIM_TIME_STEP as f64);
-            self.simulationstep_count += 1;
-
-            if self.simulation_step_duration_history.len() == SIMULATION_STEP_HISTORY_LENGTH {
-                self.simulation_step_duration_history.pop_front();
-            }
-
-            self.simulation_step_duration_history.push_back(step_processing_time);
+            self.single_sim_step(&mut current_time);
 
             // If we can't process fast enough, consume the remaining residual time.
             // I.e. we give up on getting physics-time on par to real time.
             // If we would just break here, check_update_time will keep on trying to catch up!
-            if self.simulation_processing_time.as_secs_f32() > MAX_ALLOWED_SIM_PROCESSING_TIME {
+            if self.simulation_processing_time_frame.as_secs_f32() > MAX_ALLOWED_SIM_PROCESSING_TIME {
                 while timer::check_update_time(ctx, NUM_DESIRED_SIM_UPDATES_PER_SECOND) {}
                 break;
             }
@@ -188,72 +294,14 @@ impl EventHandler for MainState {
         graphics::push_transform(ctx, Some(self.camera.transformation_matrix()));
         graphics::apply_transformations(ctx)?;
 
-        {
-            microprofile::scope!("MainState", "draw fluid");
-
-            let boundary_color = graphics::Color {
-                r: 0.2,
-                g: 0.2,
-                b: 0.2,
-                a: 1.0,
-            };
-            for (p, a) in self
-                .fluid_world
-                .particles
-                .positions
-                .iter()
-                .zip(self.fluid_world.particles.velocities.iter())
-            {
-                let c = heatmap_color((a.magnitude() * 0.1) as f32);
-                let rp: RenderPoint = RenderPoint::new(p.x, p.y);
-                graphics::draw(ctx, &self.particle_mesh, ggez::graphics::DrawParam::default().dest(rp).color(c))?;
-            }
-            for p in self.fluid_world.particles.boundary_particles.iter() {
-                let rp: RenderPoint = RenderPoint::new(p.x, p.y);
-                graphics::draw(
-                    ctx,
-                    &self.particle_mesh,
-                    ggez::graphics::DrawParam::default().dest(rp).color(boundary_color),
-                )?;
-            }
-
-            graphics::pop_transform(ctx);
-            graphics::apply_transformations(ctx)?;
-        }
-
-        {
-            microprofile::scope!("MainState", "text");
-
-            let fps = timer::fps(ctx);
-            let average_simulation_step_duration =
-                self.simulation_step_duration_history.iter().sum::<Duration>() / self.simulation_step_duration_history.len() as u32;
-
-            let fps_display = graphics::Text::new(format!(
-                "{:3.2}ms, FPS: {:3.2}\nSim duration: {:3.2}ms ({:4} steps)| Single Step (averaged): {:.2}ms\nSimTime {:.2}\nSimProcessingTime {:.2}",
-                1000.0 / fps,
-                fps,
-                self.simulation_processing_time.as_secs_f64() * 1000.0,
-                self.simulationstep_count,
-                average_simulation_step_duration.as_secs_f64() * 1000.0,
-                self.total_simulation_time.as_secs_f64(),
-                self.total_simulation_processing_time.as_secs_f64(),
-            ));
-            graphics::draw(ctx, &fps_display, (RenderPoint::new(10.0, 10.0), graphics::WHITE))?;
-
-            //if self.simulation_processing_time.as_secs_f32() / self.simulationstep_count as f32 > TIME_STEP {
-            if self.simulation_processing_time.as_secs_f32() > MAX_ALLOWED_SIM_PROCESSING_TIME {
-                graphics::draw(
-                    ctx,
-                    &graphics::Text::new("REALTIME OFF (max sim processing time hit)"),
-                    (RenderPoint::new(10.0, 70.0), graphics::Color::new(1.0, 0.2, 0.2, 1.0)),
-                )?;
-            }
-        }
+        self.draw_fluid(ctx)?;
+        self.draw_text(ctx)?;
 
         {
             microprofile::scope!("MainState", "present");
             graphics::present(ctx)?;
         }
+
         Ok(())
     }
 }
